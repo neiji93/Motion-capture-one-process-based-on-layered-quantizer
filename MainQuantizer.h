2b1dd1c9e2c9dfe498d3126d970b767f3faf274e
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-03-16 20:26:38 +0100

    Create MainQuantizer.h

---
+++ MainQuantizer.h
@@ -0,0 +1,16 @@
+//we have to check errors. so, without
+//describing a point of virw here, we can adapt quantizers
+//to forbid same distributions between themselves.
+
+class BaseQuantizer : public safeTtansformsClass //it is bedt to prevent futur errord.
+{}
+
+class FormalSeries...
+
+class MainQuantizer; //contains three layers of power supply chain
+
+class ... //thrre layers are too much consuming. then choose a technology to patch it.
+          //do't forget we didnt check previous errors.
+          //so negate two of them. on the other hand, all previous classes are usefull for pattern matching.
+
+ class ComputerVisionTdbrdf; this is the futur because we dont have models in this class. check errors too !
